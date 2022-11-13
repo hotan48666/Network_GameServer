9ae6839bbbe72964817031e32840e2cff22e6f07
hotan48666 <hotan48666@gmail.com>
2022-11-13 19:30:14 +0900

    소켓 라이브러리 추가

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+#include<WinSock2.h>
+
+#pragma comment(lib, "WS2_32.lib")
+
+using namespace std;
 
 int main()
 {
+
+	cout << "Client" << endl;
 	return 0;
 }
--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include<WinSock2.h>
+
+#pragma comment(lib, "WS2_32.lib")
+
+using namespace std;
 
 int main()
 {
+
+
+	cout << "Server" << endl;
 	return 0;
 }
